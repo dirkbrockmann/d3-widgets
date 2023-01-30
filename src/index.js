8b20c8a15fe7399378d4567b03d27709fd8012d9
Dirk Brockmannn <brockmann.dirk@gmail.com>
2023-01-30 09:55:31 +0100

    minor changes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import slider from "./slider.js"
 import toggle from "./toggle.js"
 import radio from "./radio.js"
 import widget from "./widget.js"
-import init from "./svg.js"
+import init, {svg} from "./svg.js"
 
-export {button,slider,toggle,radio,widget,grid,init}
+export {init,svg,button,slider,toggle,radio,widget,grid}
